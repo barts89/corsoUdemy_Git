c0b8b52bc04cffdd7155eb102e8530e314e6b3fc
bapadovano <bapadovano@eng.it>
2020-07-11 10:47:49 +0200

    Aggiunta funzione di divisione con controllo

--- function.php
+++ function.php
@@ -14,4 +14,14 @@ function sottrazione(n1,n2){
 function moltiplicazione(n1,n2){
 		return n1*n2;
 }
+
+/*Divisione2*/
+function divisione(n1,n2){
+	if(n2!==0){	
+		return n1*n2;
+	}
+	else {
+		return "false";
+	}
+}	
 ?>
