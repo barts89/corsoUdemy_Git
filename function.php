b6272ec62aeb338fbf142bf1a5731c2a1d72242e
bapadovano <bapadovano@eng.it>
2020-08-23 12:11:49 +0200

    Rimossi commenti e funzioni

--- function.php
+++ function.php
@@ -1,5 +1,4 @@
 <?php
-/*File realizzato appositamente per il corso di git hub base da Lucio e Mattia*/
 
 /*Addizione*/
 function addizione(n1,n2){
@@ -11,18 +10,5 @@ function sottrazione(n1,n2){
 		return n1-n2;
 }
 
-/*Moltiplicazione*/
-function moltiplicazione(n1,n2){
-		return n1*n2;
-}
 
-/*Divisione*/
-function divisione(n1,n2){
-	if(n2!==0){	
-		return n1*n2;
-	}
-	else {
-		return "false";
-	}
-}	
 ?>
